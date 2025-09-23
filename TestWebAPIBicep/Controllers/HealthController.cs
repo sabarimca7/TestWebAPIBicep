57c70ac3e60b8fd9dc7d8c20a88c122d962314b3
sabarimalai_alagar <sabarimalai_alagar>
2025-09-23 14:59:07 +0530

    Add project files.

---
+++ TestWebAPIBicep/Controllers/HealthController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestWebAPIBicep.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get() => Ok("Healthy");
+    }
+}
